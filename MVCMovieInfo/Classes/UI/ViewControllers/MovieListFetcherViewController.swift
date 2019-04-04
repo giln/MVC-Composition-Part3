7b274a099dce77c8ce3f268ea84554c917437004
Gil Nakache <gil.nakache@viseo.com>
2019-04-04 14:57:44 +0200

    Add endpoint to movieListFetcher

--- MVCMovieInfo/Classes/UI/ViewControllers/MovieListFetcherViewController.swift
+++ MVCMovieInfo/Classes/UI/ViewControllers/MovieListFetcherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MovieFetcherViewController.swift
+//  MovieListFetcherViewController.swift
 //  MVCMovieInfo
 //
 //  Created by Gil Nakache on 04/04/2019.
@@ -8,24 +8,31 @@
 
 import UIKit
 
-open class MovieFetcherViewController: UIViewController {
+open class MovieListFetcherViewController: UIViewController {
     // MARK: - Variables
 
     public var movieStore = MovieStore.shared
     private let listController = ListViewController()
+
+    public var endpoint: Endpoint = .nowPlaying {
+        didSet {
+            title = endpoint.description
+        }
+    }
 
     // MARK: - Lifecycle
 
     open override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = endpoint.description
         add(asChildViewController: listController)
     }
 
     open override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        movieStore.fetchMovies(from: .topRated, params: nil, successHandler: { [weak self] moviesResponse in
+        movieStore.fetchMovies(from: endpoint, params: nil, successHandler: { [weak self] moviesResponse in
 
             self?.listController.list = moviesResponse.results
         }) { error in
