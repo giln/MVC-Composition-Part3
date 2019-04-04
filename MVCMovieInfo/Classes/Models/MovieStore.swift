a7ec34d3ad3d5a4ffe38e19fc035d86c8536e7be
Gil Nakache <gil.nakache@viseo.com>
2019-04-04 15:45:44 +0200

    Fix Decoding Error and add Cancel on previous search

--- MVCMovieInfo/Classes/Models/MovieStore.swift
+++ MVCMovieInfo/Classes/Models/MovieStore.swift
@@ -9,145 +9,73 @@
 import Foundation
 
 public class MovieStore: MovieService {
-    
     public static let shared = MovieStore()
     private init() {}
     private let apiKey = TMDBAPIKey
     private let baseAPIURL = "https://api.themoviedb.org/3"
     private let urlSession = URLSession.shared
-    
+
+    private var searchDataTask: URLSessionDataTask?
+
     private let jsonDecoder: JSONDecoder = {
         let jsonDecoder = JSONDecoder()
         jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-mm-dd"
-        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        jsonDecoder.dateDecodingStrategy = .custom({ (decoder) -> Date in
+            // Get the string date
+            let dateString = try decoder
+                .singleValueContainer()
+                .decode(String.self)
+
+            if dateString.isEmpty {
+                return Date.distantPast
+            }
+
+            let formattedDate = dateFormatter.date(from: dateString)
+
+            guard let date = formattedDate else {
+                let context = DecodingError.Context(codingPath: [], debugDescription: "Date format was not parseable.")
+                throw DecodingError.dataCorrupted(context) // Failed to parse
+            }
+            return date
+        })
         return jsonDecoder
     }()
-    
-    
-    public func fetchMovies(from endpoint: Endpoint, params: [String: String]? = nil, successHandler: @escaping (_ response: MoviesResponse) -> Void, errorHandler: @escaping(_ error: Error) -> Void) {
-        
+
+    public func fetchMovies(from endpoint: Endpoint, params: [String: String]? = nil, successHandler: @escaping (_ response: MoviesResponse) -> Void, errorHandler: @escaping (_ error: Error) -> Void) {
         guard var urlComponents = URLComponents(string: "\(baseAPIURL)/movie/\(endpoint.rawValue)") else {
             errorHandler(MovieError.invalidEndpoint)
             return
         }
-        
+
         var queryItems = [URLQueryItem(name: "api_key", value: apiKey)]
         if let params = params {
             queryItems.append(contentsOf: params.map { URLQueryItem(name: $0.key, value: $0.value) })
         }
         urlComponents.queryItems = queryItems
-        
-        guard let url = urlComponents.url else {
-            errorHandler(MovieError.invalidEndpoint)
-            return
-        }
-        
-        urlSession.dataTask(with: url) { (data, response, error) in
-            if error != nil {
-                self.handleError(errorHandler: errorHandler, error: MovieError.apiError)
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
-                self.handleError(errorHandler: errorHandler, error: MovieError.invalidResponse)
-                return
-            }
-            
-            guard let data = data else {
-                self.handleError(errorHandler: errorHandler, error: MovieError.noData)
-                return
-            }
-            
-            do {
-                let moviesResponse = try self.jsonDecoder.decode(MoviesResponse.self, from: data)
-                DispatchQueue.main.async {
-                    successHandler(moviesResponse)
-                }
-            } catch {
-                self.handleError(errorHandler: errorHandler, error: MovieError.serializationError)
-            }
-        }.resume()
-        
-    }
-    
-    
-    public func fetchMovie(id: Int, successHandler: @escaping (_ response: Movie) -> Void, errorHandler: @escaping(_ error: Error) -> Void) {
-        guard let url = URL(string: "\(baseAPIURL)/movie/\(id)?api_key=\(apiKey)&append_to_response=videos,credits") else {
-            handleError(errorHandler: errorHandler, error: MovieError.invalidEndpoint)
-            return
-        }
-        
-        urlSession.dataTask(with: url) { (data, response, error) in
-            if error != nil {
-                self.handleError(errorHandler: errorHandler, error: MovieError.apiError)
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
-                self.handleError(errorHandler: errorHandler, error: MovieError.invalidResponse)
 
-                return
-            }
-            
-            guard let data = data else {
-                self.handleError(errorHandler: errorHandler, error: MovieError.noData)
-                return
-            }
-            
-            do {
-                let movie = try self.jsonDecoder.decode(Movie.self, from: data)
-                DispatchQueue.main.async {
-                    successHandler(movie)
-                }
-            } catch {
-                self.handleError(errorHandler: errorHandler, error: MovieError.serializationError)
-            }
-        }.resume()
-    
-    }
-    
-    func searchMovie(query: String, params: [String : String]?, successHandler: @escaping (MoviesResponse) -> Void, errorHandler: @escaping (Error) -> Void) {
-        
-        guard var urlComponents = URLComponents(string: "\(baseAPIURL)/search/movie") else {
-            errorHandler(MovieError.invalidEndpoint)
-            return
-        }
-        
-        var queryItems = [URLQueryItem(name: "api_key", value: apiKey),
-                          URLQueryItem(name: "language", value: "en-US"),
-                          URLQueryItem(name: "include_adult", value: "false"),
-                          URLQueryItem(name: "region", value: "US"),
-                          URLQueryItem(name: "query", value: query)
-                          ]
-        if let params = params {
-            queryItems.append(contentsOf: params.map { URLQueryItem(name: $0.key, value: $0.value) })
-        }
-        
-        urlComponents.queryItems = queryItems
-        
         guard let url = urlComponents.url else {
             errorHandler(MovieError.invalidEndpoint)
             return
         }
-        
-        urlSession.dataTask(with: url) { (data, response, error) in
+
+        urlSession.dataTask(with: url) { data, response, error in
             if error != nil {
                 self.handleError(errorHandler: errorHandler, error: MovieError.apiError)
                 return
             }
-            
-            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
+
+            guard let httpResponse = response as? HTTPURLResponse, 200 ..< 300 ~= httpResponse.statusCode else {
                 self.handleError(errorHandler: errorHandler, error: MovieError.invalidResponse)
                 return
             }
-            
+
             guard let data = data else {
                 self.handleError(errorHandler: errorHandler, error: MovieError.noData)
                 return
             }
-            
+
             do {
                 let moviesResponse = try self.jsonDecoder.decode(MoviesResponse.self, from: data)
                 DispatchQueue.main.async {
@@ -157,15 +85,99 @@ public class MovieStore: MovieService {
                 self.handleError(errorHandler: errorHandler, error: MovieError.serializationError)
             }
             }.resume()
-        
     }
-    
 
-    
-    private func handleError(errorHandler: @escaping(_ error: Error) -> Void, error: Error) {
+    public func fetchMovie(id: Int, successHandler: @escaping (_ response: Movie) -> Void, errorHandler: @escaping (_ error: Error) -> Void) {
+        guard let url = URL(string: "\(baseAPIURL)/movie/\(id)?api_key=\(apiKey)&append_to_response=videos,credits") else {
+            handleError(errorHandler: errorHandler, error: MovieError.invalidEndpoint)
+            return
+        }
+
+        urlSession.dataTask(with: url) { data, response, error in
+            if error != nil {
+                self.handleError(errorHandler: errorHandler, error: MovieError.apiError)
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse, 200 ..< 300 ~= httpResponse.statusCode else {
+                self.handleError(errorHandler: errorHandler, error: MovieError.invalidResponse)
+
+                return
+            }
+
+            guard let data = data else {
+                self.handleError(errorHandler: errorHandler, error: MovieError.noData)
+                return
+            }
+
+            do {
+                let movie = try self.jsonDecoder.decode(Movie.self, from: data)
+                DispatchQueue.main.async {
+                    successHandler(movie)
+                }
+            } catch {
+                self.handleError(errorHandler: errorHandler, error: MovieError.serializationError)
+            }
+            }.resume()
+    }
+
+    func searchMovie(query: String, params: [String: String]?, successHandler: @escaping (MoviesResponse) -> Void, errorHandler: @escaping (Error) -> Void) {
+        guard var urlComponents = URLComponents(string: "\(baseAPIURL)/search/movie") else {
+            errorHandler(MovieError.invalidEndpoint)
+            return
+        }
+
+        var queryItems = [URLQueryItem(name: "api_key", value: apiKey),
+                          URLQueryItem(name: "language", value: "en-US"),
+                          URLQueryItem(name: "include_adult", value: "false"),
+                          URLQueryItem(name: "region", value: "US"),
+                          URLQueryItem(name: "query", value: query)]
+        if let params = params {
+            queryItems.append(contentsOf: params.map { URLQueryItem(name: $0.key, value: $0.value) })
+        }
+
+        urlComponents.queryItems = queryItems
+
+        guard let url = urlComponents.url else {
+            errorHandler(MovieError.invalidEndpoint)
+            return
+        }
+
+        searchDataTask?.cancel()
+
+        searchDataTask = urlSession.dataTask(with: url) { data, response, error in
+            if error != nil {
+                self.handleError(errorHandler: errorHandler, error: MovieError.apiError)
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse, 200 ..< 300 ~= httpResponse.statusCode else {
+                self.handleError(errorHandler: errorHandler, error: MovieError.invalidResponse)
+                return
+            }
+
+            guard let data = data else {
+                self.handleError(errorHandler: errorHandler, error: MovieError.noData)
+                return
+            }
+
+            do {
+                let moviesResponse = try self.jsonDecoder.decode(MoviesResponse.self, from: data)
+                DispatchQueue.main.async {
+                    successHandler(moviesResponse)
+                }
+            } catch let jsonError {
+                print(jsonError)
+                self.handleError(errorHandler: errorHandler, error: MovieError.serializationError)
+            }
+        }
+        searchDataTask?.resume()
+
+    }
+
+    private func handleError(errorHandler: @escaping (_ error: Error) -> Void, error: Error) {
         DispatchQueue.main.async {
             errorHandler(error)
         }
     }
-    
 }
